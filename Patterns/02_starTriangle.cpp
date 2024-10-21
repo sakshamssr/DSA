d53b2c383df5924481406870905e6260c823faae
Saksham Singh <62788658+sakshamssr@users.noreply.github.com>
2024-10-21 23:17:58 +0530

    Create 02_starTriangle.cpp

---
+++ Patterns/02_starTriangle.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int N;
+    cout<<"Enter a Number: ";
+    cin>>N;
+    for(int i=1;i<=N;i++){
+        int j;
+        for(j=1;j<=i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
