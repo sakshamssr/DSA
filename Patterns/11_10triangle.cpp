0b2425feb6c3779fdad33f9dcc8a7ada207d0cd6
Saksham Singh <62788658+sakshamssr@users.noreply.github.com>
2024-11-08 13:32:59 +0530

    Create 11_10triangle.cpp

---
+++ Patterns/11_10triangle.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int N;
+    cout<<"Enter a Number: ";
+    cin>>N;
+    int num = 0;
+    for(int i = 0; i<N; i++){
+        if(i%2==0) num = 1;
+        for(int j = 0; j<=i; j++){
+            cout<<num;
+            num=!num;
+        }
+        cout<<endl;
+
+    }
+}
