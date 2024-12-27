d3524daa1acb2838686e8110cd35e81eac83fbfb
Saksham Singh <62788658+sakshamssr@users.noreply.github.com>
2024-12-27 13:00:48 +0530

    Create 09_factorialRec.cpp

---
+++ Recursion/09_factorialRec.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int f(int N){
+    if(N==0) return 1;
+    return N*f(N-1);
+}
+
+int main(){
+    int N;
+    cout<<"Enter a Number:";
+    cin>>N;
+    cout<<f(N);
+}
