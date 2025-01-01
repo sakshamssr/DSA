ca9dd17b23c4a479fbb197139ac8e8f230fbdb30
Saksham Singh <62788658+sakshamssr@users.noreply.github.com>
2025-01-01 12:02:03 +0530

    Create 03_mapHashing.cpp

---
+++ Hashing/03_mapHashing.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n = 7;
+    int arr[n] = {1,2,3,1,3,2,12};
+
+    //pre-compute
+    map<int, int>mpp;
+    for(int i = 0; i<=n; i++){
+        mpp[arr[i]]++;
+    }
+
+    //fetch
+    int q;
+    cout<<"Enter a Number: ";
+    cin>>q;
+    while(q--){
+        int number;
+        cout<<"Enter a Number to check: ";
+        cin>>number;
+        cout<<mpp[number]<<endl;
+    }
+}
